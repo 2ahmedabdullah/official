a740199f4bb000b55920782368c2f3869d656d8d
Abdul Ahmed <2ahmedabdullah@gmail.com>
2022-01-15 14:56:17 +0530

    Add files via upload

--- web/js/main.js
+++ web/js/main.js
@@ -1,12 +1,3 @@
-
-window.onload = function() {
-    if(!window.location.hash) {
-        window.location = window.location + '#loaded';
-        window.location.reload();
-    }
-}
-
-
 (function ($) {
     "use strict";
     
